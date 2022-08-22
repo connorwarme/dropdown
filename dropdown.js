08b0fa0dfb494f5f24a25aef845084d62736921c
connorwarme <connor.warme@gmail.com>
2022-08-21 19:30:37 -0600

    Add dropdown via js, simple version

--- dropdown.js
+++ dropdown.js
@@ -1,3 +1,11 @@
 // dropdown menu
 console.log('hello world');
 //
+const dropdown = (domElement) => {
+    domElement.style.display = "block";
+    console.log('fire');
+}
+const company = document.querySelector('button.companyBtn');
+company.addEventListener('click', () => {
+    dropdown(company.nextElementSibling);
+});
