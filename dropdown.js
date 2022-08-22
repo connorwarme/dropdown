002967512f6802d5941f85b692b8ba9a32df7c85
connorwarme <connor.warme@gmail.com>
2022-08-22 13:29:31 -0600

    Additional dropdown fnality; add icons to html

--- dropdown.js
+++ dropdown.js
@@ -13,18 +13,34 @@ const dropdown = (domElement) => {
 };
 const company = document.querySelector("button.companyBtn");
 // event listener that fires on 'click'
-company.addEventListener("click", () => {
-  dropdown(company.nextElementSibling);
+// company.addEventListener("click", () => {
+//   dropdown(company.nextElementSibling);
+// });
+// event listener (companyBtn): that fires on 'mouseover' aka hover
+company.addEventListener('mouseover', () => {
+    dropdown(company.nextElementSibling);
 });
-// window listener: any click NOT on companyBtn - hides menu
-window.onclick = (event) => {
-  if (!event.target.matches(".companyBtn")) {
-    document.querySelector("div.complist").style.display = "none";
-  }
-};
-// window listener: any mousemove NOT hovering div.company - hides menu
-window.onmousemove = () => {
-  if (!company.parentElement.matches(":hover")) {
+// event listener (companyBtn): when user moves mouse out of dom element - hide menu
+company.addEventListener('mouseleave', () => {
     company.nextElementSibling.style.display = "none";
-  }
-};
+});
+// event listener (dropdown): when user hovers over list, keep it displayed
+company.nextElementSibling.addEventListener('mouseover', () => {
+    company.nextElementSibling.style.display = "block";
+});
+// event listener (dropdown): when user moves mouse out of dropdown list - hide menu
+company.nextElementSibling.addEventListener('mouseleave', () => {
+    company.nextElementSibling.style.display = "none";
+})
+// window listener: any click NOT on companyBtn - hides menu
+// window.onclick = (event) => {
+//   if (!event.target.matches(".companyBtn")) {
+//     document.querySelector("div.complist").style.display = "none";
+//   }
+// };
+// window listener: any mousemove NOT hovering div.company - hides menu
+// window.onmousemove = () => {
+//   if (!company.parentElement.matches(":hover")) {
+//     company.nextElementSibling.style.display = "none";
+//   }
+// };
