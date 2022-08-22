fe752c2dfbdb835e80d5e72b0c4e43c1e29b954a
connorwarme <connor.warme@gmail.com>
2022-08-21 19:59:41 -0600

    Finish dropdown via js

--- dropdown.js
+++ dropdown.js
@@ -2,10 +2,24 @@
 console.log('hello world');
 //
 const dropdown = (domElement) => {
-    domElement.style.display = "block";
-    console.log('fire');
+    const drop = domElement;
+    if (drop.style.display === "block") {
+        drop.style.display = "none";
+    } else {
+        drop.style.display = "block";
+    }
 }
 const company = document.querySelector('button.companyBtn');
 company.addEventListener('click', () => {
     dropdown(company.nextElementSibling);
 });
+window.onclick = (event) => {
+    if (!event.target.matches('.companyBtn')) {
+        document.querySelector('div.complist').style.display = "none";
+    }
+}
+window.onmousemove = () => {
+    if (!(company.parentElement.matches(':hover'))) {
+        company.nextElementSibling.style.display = "none";
+    }
+}
